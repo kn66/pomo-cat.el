6586fef88445d86bd1e176a55359ca2990923e5a
flokoenig <15016743+flokoenig@users.noreply.github.com>
2025-07-28 13:46:50 +0200

    Functions to delay and stop breaks. (#1)
    * Add state indicator var to query if currently in break period
    
    * Function to quit a break
    
    * Function to delay the current break for some seconds
    
    * bring pomo-cat into focus on break
    
    ---------
    
    Co-authored-by: Florian König <florian@hunter>
    Co-authored-by: Florian König <f.koenig@ananke>

--- pomo-cat.el
+++ pomo-cat.el
@@ -34,6 +34,10 @@
   "Duration of a long break (after several cycles) in seconds."
   :type 'integer)
 
+(defcustom pomo-cat-delay-break-seconds 60
+  "Default delay for unwanted breaks."
+  :type 'integer)
+
 (defcustom pomo-cat-cat-image-path nil
   "Path to a cat image (e.g., PNG) to be shown in GUI. If nil, ASCII art is used."
   :type '(choice (const nil) file))
@@ -66,6 +70,11 @@
   "ASCII art to show when cat image is not available."
   :type 'string)
 
+(defcustom pomo-cat-get-focus nil
+  "Bring emacs' zeroth frame to the front and in focus.
+I.e. to notify the user of a break even if not working in emacs."
+  :type 'boolean)
+
 (defvar pomo-cat--timer nil
   "Internal timer object used for Pomodoro intervals.")
 
@@ -77,6 +86,9 @@
 
 (defvar pomo-cat--popon-instance nil
   "Internal popon instance for text-based cat display.")
+
+(defvar pomo-cat--in-break nil
+  "Indicator if currently in a break.")
 
 (defun pomo-cat--clear-cat-display ()
   "Clear the current cat display, whether posframe or popon."
@@ -179,12 +191,14 @@
   (setq pomo-cat--current-break-type
         (if (eq (% pomo-cat--cycle-count pomo-cat-cycles-before-long-break) 0)
             'long
-          'short))
+          'short)
+        pomo-cat--in-break t)
   (let ((duration (if (eq pomo-cat--current-break-type 'long)
                       pomo-cat-long-break-duration-seconds
                     pomo-cat-break-duration-seconds)))
     (message "Break started! (%s break)" (symbol-name pomo-cat--current-break-type))
     (pomo-cat--show-cat)
+    (when pomo-cat-get-focus (other-frame 0))
     (setq pomo-cat--timer
           (run-at-time duration nil #'pomo-cat--start-work))))
 
@@ -194,7 +208,8 @@
   (setq pomo-cat--cycle-count (1+ pomo-cat--cycle-count))
   (message "Pomodoro work #%d started!" pomo-cat--cycle-count)
   (setq pomo-cat--timer
-        (run-at-time pomo-cat-work-duration-seconds nil #'pomo-cat--start-break)))
+        (run-at-time pomo-cat-work-duration-seconds nil #'pomo-cat--start-break)
+        pomo-cat--in-break nil))
 
 ;;;###autoload
 (defun pomo-cat-start ()
@@ -212,6 +227,26 @@
   (setq pomo-cat--timer nil)
   (pomo-cat--clear-cat-display)
   (message "Pomodoro stopped."))
+
+;;;###autoload
+(defun pomo-cat-delay-break (&optional seconds)
+  "Delay the current break for `SECONDS'."
+  (interactive "P")
+  (when pomo-cat--in-break
+    (pomo-cat--clear-cat-display)
+    (cancel-timer pomo-cat--timer)
+    (let ((delay (or seconds pomo-cat-delay-break-seconds)))
+      (setq pomo-cat--timer (run-at-time delay nil #'pomo-cat--start-break))
+      (message "Break delayed %ss." delay))))
+
+;;;###autoload
+(defun pomo-cat-stop-break ()
+  "Simply stop the current break period as if it has finished."
+  (interactive)
+  (when pomo-cat--in-break
+    (pomo-cat--clear-cat-display)
+    (cancel-timer pomo-cat--timer)
+    (pomo-cat--start-work)))
 
 ;; Attempt to auto-set image path if cat.png is bundled with the file
 (unless pomo-cat-cat-image-path
